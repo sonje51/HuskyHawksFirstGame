1d9b581da0953573321c5b7abd7ddd41d76432ee
Tanisha Thakare <156538168+tanisha-r-thakare@users.noreply.github.com>
2026-03-12 00:27:22 +0000

    Added Comments to the Ball.h and Ball.cpp files

--- src/Ball.h
+++ src/Ball.h
@@ -1,21 +1,36 @@
+//Ball.h
+
 #ifndef BALL_H
 #define BALL_H
 
 #include <SFML/Graphics.hpp>
 
+/*
+// Ball Class
+// Represents the ball in a breakout-style game. Handles rendering, movement,
+// and collisions with the paddle. The ball moves within a constrained window
+// and bounces off walls and the paddle.
+*/
 class Ball
 {
+
+/*
+// Constructor
+//
+// _window: Pointer to the SFML window where the ball will be drawn.
+// WIDTH, HEIGHT: Dimensions of the game window, used to set movement limits.
+*/
 public:
     Ball(sf::RenderWindow *_window, unsigned int WIDTH, unsigned int HEIGHT);
-    void drawBall();
-    void move(float deltaTime);
-    void checkPadCollision(sf::FloatRect padBounds);
+    void drawBall(); //Draws the ball on the window using the SFML CircleShape.
+    void move(float deltaTime); //Moves the ball according to its speed and current direction.
+    void checkPadCollision(sf::FloatRect padBounds); //Checks if the ball collides with the paddle and adjusts direction if it does.
 
+//Variables
 private:
     sf::RenderWindow *window;
-
-    const int ballRadius;
-    float posY;
+    const int ballRadius; 
+    float posY; 
     float posX;
     const float minPosX;
     const float maxPosX;
